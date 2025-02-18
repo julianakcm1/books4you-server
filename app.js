9f4717eaf7f340d677b4e4b398c182afee1782fd
julianakcm1 <jkcm@ic.ufal.br>
2025-02-17 22:14:39 -0300

    feat: add cors

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bookRoute = require('./routes/book')
 const app = express()
 app.use(express.json()) // reading JSON
 
+const cors = require("cors");
+app.use(cors());
+
 const port = 8000
 
 app.use('/books', bookRoute)
